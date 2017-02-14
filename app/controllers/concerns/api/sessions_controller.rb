61b51a2391020f428ac89a8dbf8287854c36f868
Mike Boan <mikeboan@gmail.com>
2017-02-14 10:21:22 -0500

    Complete backend auth

---
+++ app/controllers/concerns/api/sessions_controller.rb
@@ -0,0 +1,61 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      log_in!(@user)
+      render "api/users/show"
+    else
+      render json: { base: ["Invalid username/password"] }, status: 422
+    end
+  end
+
+  def show
+    @user = current_user
+    if @user
+      render "api/users/show"
+    else
+      render json: nil
+    end
+  end
+
+  def destroy
+    @user = current_user
+    log_out!
+    render "api/users/show"
+  end
+
+end
+
+## AJAX requests for testing from browser console
+# $.ajax({
+#   url: '/api/users',
+#   method: 'POST',
+#   data: {
+#     user: {
+#       username: 'mike',
+#       password: 'starwars'
+#     }
+#   }
+# }).then(data => console.log(data));
+#
+# $.ajax({
+#   url: '/api/session',
+#   method: 'GET',
+# }).then(data => console.log(data));
+#
+# $.ajax({
+#   url: '/api/session',
+#   method: 'DELETE'
+# }).then(data => console.log(data));
+#
+# $.ajax({
+#   url: '/api/session',
+#   method: 'POST',
+#   data: {
+#     user: {
+#       username: 'mike',
+#       password: 'starwars'
+#     }
+#   }
+# }).then(data => console.log(data));
---
+++ app/controllers/concerns/api/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      render :show
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+end
